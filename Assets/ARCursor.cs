ca947349e4950d117a3c0e9b4f23cf28e312641c
Novaborn-dev <68510977+Novaborn-dev@users.noreply.github.com>
2021-03-06 14:54:22 -0600

    initial commit

---
+++ Assets/ARCursor.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ARCursor : MonoBehaviour
+{
+    public GameObject cursorChildObject;
+    public GameObject objectToPlace;
+    public ARRaycastManager raycastManager;
+
+    public bool useCursor = true;
+
+    void Start()
+    {
+        cursorChildObject.SetActive(useCursor);
+    }
+
+    void Update()
+    {
+        if (useCursor)
+        {
+            UpdateCursor();
+        }
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            if (useCursor)
+            {
+                GameObject.Instantiate(objectToPlace, transform.position, transform.rotation);
+            }
+            else
+            {
+                List<ARRaycastHit> hits = new List<ARRaycastHit>();
+                raycastManager.Raycast(Input.GetTouch(0).position, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
+                if (hits.Count > 0)
+                {
+                    GameObject.Instantiate(objectToPlace, hits[0].pose.position, hits[0].pose.rotation);
+                }
+            }
+        }
+    }
+
+    void UpdateCursor()
+    {
+        Vector2 screenPosition = Camera.main.ViewportToScreenPoint(new Vector2(0.5f, 0.5f));
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        raycastManager.Raycast(screenPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
+
+        if (hits.Count > 0)
+        {
+            transform.position = hits[0].pose.position;
+            transform.rotation = hits[0].pose.rotation;
+        }
+    }
+}
